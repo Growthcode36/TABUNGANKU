d7f7a2df67246440635ecc266066068b8bddb645
Growthcode36 <nngodingbareng@gmail.com>
2025-02-27 11:36:29 +0700

    Update and rename Node.js to server.js

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
+
+let userLocation = null;
 
 app.post('/save-location', (req, res) => {
     const { latitude, longitude } = req.body;
+    userLocation = { latitude, longitude };
     console.log(`Lokasi diterima: Latitude = ${latitude}, Longitude = ${longitude}`);
     res.send("Lokasi diterima!");
+});
+
+app.get('/get-location', (req, res) => {
+    if (userLocation) {
+        res.json(userLocation);
+    } else {
+        res.status(404).send("Belum ada lokasi yang diterima.");
+    }
 });
 
 app.listen(port, () => {
